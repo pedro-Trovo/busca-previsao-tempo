6e9e405fc2604a54b0027a0c0b8ae1ade1abd3ab
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-06-07 00:48:17 -0300

    feat(p2): cria componente busca

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.min.css';
+import Busca from './components/Busca';
 
 function App() {
-  return <div>Limpando...</div>;
+  return (
+    <div>
+      <Busca />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Busca.jsx
@@ -0,0 +1,28 @@
+const Busca = () => {
+  return (
+    <div className="relative w-min">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#969696"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-search absolute top-0 bottom-0 my-auto ml-2"
+      >
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      </svg>
+      <input
+        type="text"
+        className="py-2 pl-6 pr-2 w-15rem text-base"
+        placeholder="Nome de uma cidade...."
+      />
+    </div>
+  );
+};
+
+export default Busca;

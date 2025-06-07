6a620c48a574f7ffba21b16e6d957d133a494c03
Pedro Trovo <trovopedro5@gmail.com>
2025-06-07 01:53:14 -0300

    feat(p2): implementa endpoint de previsão do tempo

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const axios = require('axios')
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+app.use(cors())
+
+const weatherClient = axios.create({
+  baseURL: 'https://api.openweathermap.org/data/2.5/',
+  params: {
+    appid: process.env.OPENWEATHER_KEY,
+    units: 'metric',
+    lang: 'pt_br'
+  }
+})
+
+app.get('/search', async (req, res) => {
+  const query = req.query.query  
+  const result = await weatherClient.get('forecast', {
+    params: { q: query }  
+  })
+
+  const dadosFiltrados = result.data.list.map(item => ({
+    temp_min: item.main.temp_min,
+    temp_max: item.main.temp_max,
+    umidade: item.main.humidity,
+    icon: item.weather[0].icon,
+    descricao: item.weather[0].description
+  }))
+
+  res.json(dadosFiltrados)
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Back End OK! Porta ${port}.`))
